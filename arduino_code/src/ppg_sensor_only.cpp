1f349771eae637f6cdd8b1f7e0f70ac42a2194bf
= <=>
2026-02-12 17:45:27 -0500

    clean up code - to be tested

--- arduino_code/src/ppg_sensor_only.cpp
+++ arduino_code/src/ppg_sensor_only.cpp
@@ -1,22 +1,8 @@
 #include <Arduino.h>
 
-// Pin Definitions
-const int POLAR_PIN = 7;
-const int PULSE_SENSOR_PIN = A0;  // Pulse Sensor PURPLE WIRE connected to ANALOG PIN A0
+// Pin Definitions (Arduino Nano ESP32)
+const int PULSE_SENSOR_PIN = A0;  // Analog input pin for the pulse sensor
 const int LED13 = LED_BUILTIN;    // On-board Arduino LED
-
-// Polar Sensor Variables
-byte polarOldSample, polarSample;
-int polarBeatCount = 0;
-unsigned long polarLastBeatTime = 0;
-unsigned long polarCurrentBeatTime = 0;
-unsigned long polarBeatInterval = 0;
-int polarBPM = 0;
-const int polarNumReadings = 5;
-int polarBpmReadings[polarNumReadings];
-int polarReadIndex = 0;
-int polarBpmTotal = 0;
-int polarBpmAverage = 0;
 
 // PulseSensor Variables
 int Signal;                     // holds the incoming raw data. Signal value can range from 0-4095 on ESP32
@@ -35,8 +21,6 @@ portMUX_TYPE sampleMux = portMUX_INITIALIZER_UNLOCKED;
 hw_timer_t *sampleTimer = nullptr;
 
 // Function Prototypes
-void setupPolarSensor();
-void readPolarSensor();
 void setupPulseSensor();
 void readPulseSensor();
 void setupTimer100Hz();
@@ -59,65 +43,12 @@ void setupTimer100Hz() {
 
 void setup() {
   Serial.begin(115200);
-  //setupPolarSensor();
   setupPulseSensor();
   setupTimer100Hz();
 }
 
 void loop() {
-  //readPolarSensor();
   readPulseSensor();
-}
-
-// ========== POLAR SENSOR FUNCTIONS ==========
-
-void setupPolarSensor() {
-  pinMode(POLAR_PIN, INPUT);
-  Serial.println("Waiting for heart beat...");
-
-  // Initialize BPM readings array
-  for (int i = 0; i < polarNumReadings; i++) {
-    polarBpmReadings[i] = 0;
-  }
-
-  // Wait until a heart beat is detected
-  while (!digitalRead(POLAR_PIN)) {};
-  Serial.println("Heart beat detected!");
-  polarLastBeatTime = millis();
-}
-
-void readPolarSensor() {
-  polarSample = digitalRead(POLAR_PIN);
-
-  if (polarSample && (polarOldSample != polarSample)) {
-    // Beat detected
-    polarCurrentBeatTime = millis();
-    polarBeatInterval = polarCurrentBeatTime - polarLastBeatTime;
-    polarLastBeatTime = polarCurrentBeatTime;
-
-    // Calculate BPM from interval (60000 ms = 1 minute)
-    polarBPM = 60000 / polarBeatInterval;
-
-    // Add to running average
-    polarBpmTotal = polarBpmTotal - polarBpmReadings[polarReadIndex];
-    polarBpmReadings[polarReadIndex] = polarBPM;
-    polarBpmTotal = polarBpmTotal + polarBpmReadings[polarReadIndex];
-    polarReadIndex = (polarReadIndex + 1) % polarNumReadings;
-    polarBpmAverage = polarBpmTotal / polarNumReadings;
-
-    // Output results - ONLY when beat is detected, ONLY after 5 beats
-    if (polarBeatCount > 5) {
-      Serial.print(">");
-      Serial.print("PolarRealtimeBPM:");
-      Serial.print(polarBPM);
-      Serial.print(",PolarBPM:");
-      Serial.print(polarBpmAverage);
-      Serial.println();
-    }
-    polarBeatCount++; // iterate number of beats
-  }
-
-  polarOldSample = polarSample;
 }
 
 // ========== PULSESENSOR FUNCTIONS ==========
